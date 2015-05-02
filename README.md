8a8d44e54f77622623acf7403244d35143efc21b
Alex Sanders <a@snd.rs>
2015-05-02 16:53:17 +0100

    add initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# JSPM + gulp + react
+
+Simple react shell app, using JSPM and gulp.
+
+## Installation
+- `npm install && jspm install`
+
+## Development
+- `gulp dev` – uses browser-sync to reload after edits
+
+## Build
+- `gulp build` creates a rev'd copy of the source in `/dist`
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+var del = require('del');
+var shell = require('gulp-shell');
+var RevAll = require('gulp-rev-all');
+var htmlreplace = require('gulp-html-replace');
+
+// DEV TASKS
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        "files": [
+            "*.html",
+            "js/*"
+        ]
+    });
+});
+
+gulp.task('dev', ['browser-sync']);
+
+// BUILD TASKS
+gulp.task('clean', function (cb) {
+  del(['dist'], cb);
+});
+
+gulp.task('html', function () {
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'js': {
+                src: 'js/app.js',
+                tpl: '<script src="%s" async defer></script>'
+            }
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', shell.task([
+  'jspm bundle-sfx js/main dist/js/app.js --minify'
+]));
+
+gulp.task('rev', function() {
+    var revAll = new RevAll({ dontRenameFile: ['.html'] });
+    gulp.src('dist/**')
+        .pipe(revAll.revision())
+        .pipe(gulp.dest('dist'))
+        .pipe(revAll.manifestFile())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function () {
+    runSequence(
+        'clean',
+        ['html', 'js'],
+        'rev'
+    );
+});
