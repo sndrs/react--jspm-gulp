646b56a8aa260bfcace0dc15513788fd812a757e
Alex Sanders <a@snd.rs>
2015-05-03 14:23:51 +0100

    what's this doing here?

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ import React from 'react';
 import domReady from 'domready';
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
-    }
-
     render () {
         return (
             <div>hello</div>
